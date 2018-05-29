e3b4134e70bfc96e52e1517182233c4efe6834ed
ha.thi.hoan <ha.thi.hoan@vn-macbook-0095s-Mac-mini.local>
2018-05-29 14:43:47 +0700

    [Hoan][Login] Create UI for login

---
+++ FacebookNewFeeds/FacebookNewFeeds/NewFeedsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NewFeedsViewController.swift
+//  FacebookNewFeeds
+//
+//  Created by ha.thi.hoan on 5/29/18.
+//  Copyright © 2018 ha.thi.hoan. All rights reserved.
+//
+
+import UIKit
+
+class NewFeedsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ FacebookNewFeeds/FacebookNewFeeds/RegisterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  RegisterViewController.swift
+//  FacebookNewFeeds
+//
+//  Created by ha.thi.hoan on 5/28/18.
+//  Copyright © 2018 ha.thi.hoan. All rights reserved.
+//
+
+import UIKit
+
+private struct Constants {
+    public static let goToMainSegue = "goToMain"
+}
+
+class RegisterViewController: UIViewController {
+
+    @IBOutlet weak var accountNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repasswordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func registerAction(_ sender: Any) {
+        if accountNameTextField.text != "" && passwordTextField.text != "" && repasswordTextField.text != "" {
+            performSegue(withIdentifier: Constants.goToMainSegue, sender: nil)
+        }
+    }
+}
